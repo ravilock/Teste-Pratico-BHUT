f61f7e0abf1fa97e646521e10087d80d6073f713
ravilock <ravi.me.professional@hotmail.com>
2021-07-01 01:05:19 -0300

    BHUT_TI

---
+++ BHUT_TI.js
@@ -0,0 +1,42 @@
+class Range {
+    constructor(limit) {
+        this.values = [...Array(limit).keys()].map(i => i + 1)
+    }
+
+    get list() {
+        return this.values
+    }
+}
+
+class FiltersByMultiple {
+    constructor(...NumbersNTexts) {
+        this.NumberToText = {}
+        NumbersNTexts.map(x => this.NumberToText[Object.keys(x)[0]] = Object.values(x)[0])
+    }
+
+    get numberTextRelation() {
+        return this.NumberToText
+    }
+}
+
+class FilterApplier {
+    constructor(range, filter) {
+        range.list.map(x => console.log(this.applyFilter(x, filter.numberTextRelation)))
+    }
+
+    applyFilter(number, filter) {
+        let divisibleCheck = [];
+        let value;
+        for (let [key, values] of Object.entries(filter)) {
+            divisibleCheck.push((number%key == 0)*key)
+        }
+        divisibleCheck.forEach(element => {
+            value = filter[element] || value || number
+        });
+        return value + ' - ' + number
+    }
+}
+
+let range = new Range(100)
+let filters = new FiltersByMultiple({3: 'BHUT'}, {5: 'IT'}, {15: 'BHUT TI'})
+let applier = new FilterApplier(range, filters)
